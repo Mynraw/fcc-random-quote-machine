10fcb61d18a618d5a2230cabc8e0ccf24078ade9
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-03-24 14:38:36 +0300

    dispatch added

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -1,28 +1,60 @@
-import { FaTwitterSquare, FaTumblrSquare, FaQuoteLeft } from "react-icons/fa"
+import { useDispatch, useSelector } from "react-redux";
+import { themePalette } from "../redux/quoteSlice";
+import { FaTwitterSquare, FaTumblrSquare, FaQuoteLeft } from "react-icons/fa";
+
+const colorPalette = [
+  "#42032C",
+  "#D36B00",
+  "#4C6793",
+  "#0F3D3E",
+  "#51557E",
+  "#B85252",
+  "#5C527F",
+  "#FFAA4C",
+  "#393E46",
+  "#10684A",
+];
 
 const QuoteCard = () => {
+  const dispatch = useDispatch();
+  const paletteColor = useSelector(themePalette);
+
+  const handleClick = () => {
+    dispatch(themePalette(Math.floor(Math.random() * 10)));
+    console.log(paletteColor);
+  };
 
   return (
     <section className="bg-orange-500 w-fit rounded-lg">
-    <div className="px-14 py-8 flex flex-col gap-y-10">
-      <div>
-        <div className="flex w-[500px]">
-        <FaQuoteLeft className="w-16 h-9" /> <p className="text-3xl inline whitespace-normal pl-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, quasi?</p>
+      <div className="px-14 py-8 flex flex-col gap-y-10">
+        <div>
+          <div className="flex w-[500px]">
+            <FaQuoteLeft className="w-16 h-9" />{" "}
+            <p className="text-3xl inline whitespace-normal pl-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa,
+              quasi?
+            </p>
+          </div>
+          <p className="text-right">- Lorem, ipsum.</p>
         </div>
-        <p className="text-right">- Lorem, ipsum.</p>
+        <div className="flex items-center justify-between">
+          <div className="flex">
+            <FaTwitterSquare className="w-10 h-10" />
+            <FaTumblrSquare className="w-10 h-10" />
+          </div>
+          <div>
+            <button
+              onClick={handleClick}
+              type="button"
+              className="capitalize px-4 py-1.5 bg-white rounded-[4px]"
+            >
+              new quote
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="flex items-center justify-between">
-      <div className="flex">
-      <FaTwitterSquare className="w-10 h-10" />
-      <FaTumblrSquare className="w-10 h-10" />
-      </div>
-      <div>
-        <button className="capitalize px-4 py-1.5 bg-white rounded-[4px]">new quote</button>
-      </div>
-      </div>
-    </div>
     </section>
-  )
-}
+  );
+};
 
-export default QuoteCard
+export default QuoteCard;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@component/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@component/styles/globals.css";
+import { Provider } from "react-redux";
+import store from "@component/redux/store";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
