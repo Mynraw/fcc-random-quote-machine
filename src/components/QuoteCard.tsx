a4c1bb52c0320e92dd3b6cd5a6d72386cea7bf0a
Arya Acikgoz <arya.acikgoz@ankaref.com>
2023-03-24 11:41:55 +0300

    card template

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -1,6 +1,26 @@
+import { FaTwitterSquare, FaTumblrSquare, FaQuoteLeft } from "react-icons/fa"
+
 const QuoteCard = () => {
+  
   return (
-    <div>QuoteCard</div>
+    <div className="px-14 py-8 flex flex-col gap-y-10">
+      <div>
+        <div className="flex w-[500px]">
+        
+        <FaQuoteLeft className="w-16 h-9" /> <p className="text-3xl inline whitespace-normal pl-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, quasi?</p>
+        </div>
+        <p className="text-right">- Lorem, ipsum.</p>
+      </div>
+      <div className="flex items-center justify-between">
+      <div className="flex">
+      <FaTwitterSquare className="w-10 h-10" />
+      <FaTumblrSquare className="w-10 h-10" />
+      </div>
+      <div>
+        <button className="capitalize px-4 py-1.5 bg-white rounded-[4px]">new quote</button>
+      </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,12 @@ import QuoteCard from "@component/components/QuoteCard"
 
 export default function Home() {
   return (
-    <main id="quote-box" className="w-full h-full">
-      <div className="container mx-auto">
-        <section>
+    <main id="quote-box" className="grid place-items-center w-screen h-screen">
+      <div className="container m-auto grid place-items-center">
+        <section className="bg-orange-500 w-fit rounded-lg">
           <QuoteCard />
         </section>
+        <footer className="text-center">asd</footer>
       </div>
     </main>
   )
